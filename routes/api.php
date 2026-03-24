084c4f88f4fd335224426f7b4ed8d97b6bd0911a
skazo4nik <demomailbk.ru@gmail.com>
2026-03-24 17:50:56 +1000

    Ещё чуть API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,11 +15,14 @@ Route::get('/docs', function () {
     //exec('php artisan spectrum:generate --format=html');
     return redirect()->to('/spectrum/openapi.html');
 });
+
 //TODO: Перенести часть из котроллеров в API
+//TODO: Почему-то документация создаётся оп шаблону api/api/*
 
 // Public routes (без авторизации)
 Route::post('/register', [AuthController::class, 'register'])->name('api.register');
 Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+Route::get('/test', [PostController::class, 'fetchAll'])->name('api.posts.fetchAll');
 
 // Protected routes (требуют авторизации)
 Route::middleware('auth:sanctum')->group(function () {
@@ -28,9 +31,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user'])->name('api.user');
 
     // Posts
-    Route::get('/posts', [PostController::class, 'index'])->name('api.posts.index');
-    Route::post('/posts', [PostController::class, 'store'])->name('api.posts.store');
-    Route::get('/posts/{post}', [PostController::class, 'show'])->name('api.posts.show');
+    Route::get('/posts', [PostController::class, 'fetchAll'])->name('api.posts.fetchAll');
+    Route::get('/posts/{post}', [PostController::class, 'fetch'])->name('api.posts.fetch');
+    Route::post('/posts', [PostController::class, 'create'])->name('api.posts.create');
     Route::put('/posts/{post}', [PostController::class, 'update'])->name('api.posts.update');
-    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('api.posts.destroy');
+    Route::delete('/posts/{post}', [PostController::class, 'kill'])->name('api.posts.kill');
 });
